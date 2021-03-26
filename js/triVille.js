85acb73884f3c5b5a0bed801f51f09ad731f6f79
Tifanny SERAFIN <tifanny.serafin@Digital-Grenoble.local>
2021-03-26 15:50:16 +0100

    fonction distance gnb

--- js/triVille.js
+++ js/triVille.js
@@ -61,8 +61,19 @@ function getArrayCsv(csv) {
  * @returns la distance qui sépare la ville de Grenoble
  */
 function distanceFromGrenoble(ville) {
-    console.log('implement me !');
-    return 0;
+    const R = 6371e3; // metres
+    const φ1 = 45.188529 * Math.PI/180; // φ, λ in radians
+    const φ2 = ville.latitude * Math.PI/180;
+    const Δφ = (ville.latitude-45.188529) * Math.PI/180;
+    const Δλ = (ville.longitude-5.724524) * Math.PI/180;
+
+    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    const d = R * c; // in metres
+    return d;
 }
 
 /**
